b0babbf25ce979b6a93b548f62b8d95f98a6632b
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:18:07 -0500

    test refactor

--- speakermeet-spa/src/services/mockSpeakerService.spec.js
+++ speakermeet-spa/src/services/mockSpeakerService.spec.js
@@ -1,43 +1,45 @@
 import { expect } from 'chai';
 
 describe('Mock Speaker Service', () => {
-  it('exits', () => {
-    expect(MockSpeakerService).to.exist;
-  });
-
-  it('can be constructed', () => {
-    // arrange
-    let service = new MockSpeakerService();
-
-    // assert
-    expect(service).to.be.an.instanceof(MockSpeakerService);
-  });
-
-  describe('Get All', () => {
-    it('exists', () => {
+    it('exits', () => {
+      expect(MockSpeakerService).to.exist;
+    });
+  
+    it('can be constructed', () => {
       // arrange
       let service = new MockSpeakerService();
-
+  
       // assert
-      expect(service.getAll).to.exist;
+      expect(service).to.be.an.instanceof(MockSpeakerService);
     });
-
-    describe('No Speakers Exist', () => {
-      it('returns an empty array', () => {
-        // arrange
-        let service = new MockSpeakerService();
-
-        // act
-        let promise = service.getAll();
-
-        // assert
-        return promise.then(result => {
-          expect(result).to.have.lengthOf(0);
+  
+    describe('After Initialization', () => {
+      let service = null;
+  
+      beforeEach(() => {
+        service = new MockSpeakerService();
+      });
+  
+      describe('Get All', () => {
+        it('exists', () => {
+          // assert
+          expect(service.getAll).to.exist;
+        });
+  
+        describe('No Speakers Exist', () => {
+          it('returns an empty array', () => {
+            // act
+            let promise = service.getAll();
+  
+            // assert
+            return promise.then((result) => {
+              expect(result).to.have.lengthOf(0);
+            });
+          });
         });
       });
     });
   });
-});
 
 class MockSpeakerService {
   getAll() {
