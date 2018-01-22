8716e89964bdfa6993f9eeee1623f9101fbe9602
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:54:20 -0500

    Speaker Page

---
+++ speakermeet-spa/src/speakers/SpeakersPage.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import { SpeakersPage } from './SpeakersPage';
+
+describe('Speakers Page', () => {
+  it('exists', () => {
+    expect(SpeakersPage).to.exist;
+  });
+});
