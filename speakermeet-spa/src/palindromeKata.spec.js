d967090cd41c4827ae77287dda3fdd76f80705b6
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 20:40:25 -0500

    Palindrome checks 2 non-matching letters

--- speakermeet-spa/src/palindromeKata.spec.js
+++ speakermeet-spa/src/palindromeKata.spec.js
@@ -1,28 +1,43 @@
 import { expect } from 'chai';
 
 describe('Test Framework', () => {
-    it('is configured correctly', () => {
-        expect(1).to.equal(1);
-    });
+  it('is configured correctly', () => {
+    expect(1).to.equal(1);
+  });
 });
 
-describe('Is Palindrome', ()=> {
-    it('exists', () => {
-        expect(isPalindrome).to.exist;
-    });
+describe('Is Palindrome', () => {
+  it('exists', () => {
+    expect(isPalindrome).to.exist;
+  });
 
-    it('a single letter is a palindrome', () => {
-        // arrange
-        const value = 'a';
-       
-        // act
-        const result = isPalindrome(value);
-       
-        // assert
-        expect(result).to.be.true;
-      });
+  it('a single letter is a palindrome', () => {
+    // arrange
+    const value = 'a';
+
+    // act
+    const result = isPalindrome(value);
+
+    // assert
+    expect(result).to.be.true;
+  });
+
+  it('two non-matching letters is not a palindrome', () => {
+    // arrange
+    const value = 'at';
+
+    // act
+    const result = isPalindrome(value);
+
+    // assert
+    expect(result).to.be.false;
+  });
 });
 
-function isPalindrome() {
+function isPalindrome(value) {
+  if (value.length === 1) {
     return true;
+  }
+
+  return false;
 }
