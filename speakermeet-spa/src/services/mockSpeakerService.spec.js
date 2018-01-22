18e0aba38cfdc7638d5c614cfef1c5c561cc9234
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:14:05 -0500

    Mock speaker service can be constructed

--- speakermeet-spa/src/services/mockSpeakerService.spec.js
+++ speakermeet-spa/src/services/mockSpeakerService.spec.js
@@ -4,6 +4,15 @@ describe('Mock Speaker Service', () => {
   it('exits', () => {
     expect(MockSpeakerService).to.exist;
   });
+
+  it('can be constructed', () => {
+    // arrange
+    let service = new MockSpeakerService();
+   
+    // assert
+    expect(service).to.be.an.instanceof(MockSpeakerService);
+  });
 });
 
-let MockSpeakerService = {};
+class MockSpeakerService {
+}
