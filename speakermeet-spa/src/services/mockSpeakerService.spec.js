c11a6701be020d7214537a39608250c083ca8990
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:15:51 -0500

    Mock speaker service has getAll

--- speakermeet-spa/src/services/mockSpeakerService.spec.js
+++ speakermeet-spa/src/services/mockSpeakerService.spec.js
@@ -8,11 +8,22 @@ describe('Mock Speaker Service', () => {
   it('can be constructed', () => {
     // arrange
     let service = new MockSpeakerService();
-   
+
     // assert
     expect(service).to.be.an.instanceof(MockSpeakerService);
+  });
+
+  describe('Get All', () => {
+    it('exists', () => {
+      // arrange
+      let service = new MockSpeakerService();
+
+      // assert
+      expect(service.getAll).to.exist;
+    });
   });
 });
 
 class MockSpeakerService {
+  getAll() {}
 }
