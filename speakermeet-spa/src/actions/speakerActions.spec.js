bd9f7d9f3896c9f76a4c299e1db5dd27fb0e581b
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:23:45 -0500

    Speaker actions - getSpeakerSuccess returns correctly

--- speakermeet-spa/src/actions/speakerActions.spec.js
+++ speakermeet-spa/src/actions/speakerActions.spec.js
@@ -6,9 +6,31 @@ describe('Speaker Actions', () => {
       it('exists', () => {
         expect(getSpeakersSuccess).to.exist;
       });
+
+      it('is created with correct data', () => {
+        // arrange
+        const speakers = [
+          {
+            id: 'test-speaker',
+            firstName: 'Test',
+            lastName: 'Speaker'
+          }
+        ];
+
+        // act
+        const result = getSpeakersSuccess(speakers);
+
+        // assert
+        expect(result.type).to.equal(GET_SPEAKERS_SUCCESS);
+        expect(result.speakers).to.have.lengthOf(1);
+        expect(result.speakers).to.deep.equal(speakers);
+      });
     });
   });
 });
 
-function getSpeakersSuccess() {
+const GET_SPEAKERS_SUCCESS = 'GET_SPEAKERS_SUCCESS';
+
+function getSpeakersSuccess(speakers) {
+  return { type: GET_SPEAKERS_SUCCESS, speakers: speakers };
 }
