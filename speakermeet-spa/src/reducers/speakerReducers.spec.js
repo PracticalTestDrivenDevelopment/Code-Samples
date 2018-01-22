9ef743825065f2b08153c56717c4c7734d45642f
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 22:49:52 -0500

    Loading Speakers

--- speakermeet-spa/src/reducers/speakerReducers.spec.js
+++ speakermeet-spa/src/reducers/speakerReducers.spec.js
@@ -1,11 +1,39 @@
 import { expect } from 'chai';
+import * as actions from '../actions/speakerActions';
+import * as types from './actionTypes';
 
 describe('Speaker Reducers', () => {
   describe('Speakers Reducer', () => {
     it('exists', () => {
       expect(speakersReducer).to.exist;
     });
+
+    it('Loads Speakers', () => {
+      // arrange
+      const initialState = [];
+
+      const speaker = {
+        id: 'test-speaker',
+        firstName: 'Test',
+        lastName: 'Speaker'
+      };
+      const action = actions.getSpeakersSuccess([speaker]);
+
+      // act
+      const newState = speakersReducer(initialState, action);
+
+      // assert
+      expect(newState).to.have.lengthOf(1);
+      expect(newState[0]).to.deep.equal(speaker);
+    });
   });
 });
 
-function speakersReducer() {}
+function speakersReducer(state = [], action) {
+  switch (action.type) {
+    case types.GET_SPEAKERS_SUCCESS:
+      return action.speakers;
+    default:
+      return state;
+  }
+}
