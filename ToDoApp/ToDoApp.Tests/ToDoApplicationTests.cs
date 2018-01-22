9bed968fe7aa566f683f84436c85675b83460f9a
Clayton Hunt <claytonhunt_104@hotmail.com>
2018-01-21 21:34:32 -0500

    Getting Todos

--- ToDoApp/ToDoApp.Tests/ToDoApplicationTests.cs
+++ ToDoApp/ToDoApp.Tests/ToDoApplicationTests.cs
@@ -11,10 +11,25 @@ namespace ToDoApp.Tests
 
             Assert.NotNull(todo);
         }
+
+        [Fact]
+        public void CanGetTodos()
+        {
+            // Arrange
+            var todo = new TodoList();
+
+            // Act
+            var result = todo.Items;
+
+            // Assert
+            Assert.NotNull(result);
+        }
     }
 
     internal class TodoList
     {
+        public object Items { get; } = new object();
+
         public TodoList()
         {
         }
